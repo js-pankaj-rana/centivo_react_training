bc2baf30483a6c9b7a74a97151864cc58b28f03a
Pankaj Rana <pankaj.rana@rsystems.com>
2020-03-03 20:18:06 +0530

    Day 1: Basic Understanding of React JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+        count: 0,
+        rendering: false
+    }
+  }
+
+  handleIncrement = () => {
+    // this.state.count += 1; 
+    this.setState( (prevState) => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  handleDecrement = () => {
+    this.setState( (prevState) => {
+      return {
+        count:  prevState.count - 1
+      }
+    })
+  }
+
+
+  render(){
+      return (
+        <>
+          <center>
+          <div className="counter-view">{this.state.count}</div>
+          <button type="button" className="btn btn-primary" onClick={this.handleIncrement}>Count +</button>
+          <button type="button" className="btn btn-secondary" onClick={this.handleDecrement}>Count -</button>
+          </center>
+        </>
+      )
+  }
 }
+
+
 
 export default App;
